048e03e334742970af266012ce56ec57ee883675
AndreaJaramilloL <valeriavenegas844@gmail.com>
2021-02-03 14:35:54 -0600

    holamundo2

---
+++ 2holamundo/Program.cs
@@ -0,0 +1,14 @@
+﻿//este es el primer programa holamundo
+using System;
+
+namespace _2holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Programación C#!");
+        }
+    }
+}
