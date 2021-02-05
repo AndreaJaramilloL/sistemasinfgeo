359c50d97886046fdd529acbd5e3e8ff59815636
AndreaJaramilloL <valeriavenegas844@gmail.com>
2021-02-04 21:33:24 -0600

    7tablas

---
+++ 7tablas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rango;
+            Console.WriteLine("Introducir hasta que rango se desea que leue la tabla");
+            rango=Convert.ToInt16(Console.ReadLine());
+            for(int i=1; i<=rangoi++)
+            {
+                for(int j = 5; j<=11; j++)
+                Console.WriteLine(i + "x" + j + "=" + (int i * j));
+                Console.ReadKey();
+            }
+        }
+    }
+}
