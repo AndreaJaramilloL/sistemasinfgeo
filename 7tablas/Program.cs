239f236f690faf57b136ca84f80d8f555983c72d
AndreaJaramilloL <valeriavenegas844@gmail.com>
2021-02-04 22:11:21 -0600

    7tabla

--- 7tablas/Program.cs
+++ 7tablas/Program.cs
@@ -1,20 +1,27 @@
-﻿using System;
-
+﻿//programa que muestre tablas de multiplicar 
+//Andrea Jaramillo Leiva
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace _7tablas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int rango;
-            Console.WriteLine("Introducir hasta que rango se desea que leue la tabla");
-            rango=Convert.ToInt16(Console.ReadLine());
-            for(int i=1; i<=rangoi++)
+           int r;
+            Console.WriteLine("indica que tabla desea visualizar");  
+            
+            r = Convert.ToInt16(Console.ReadLine());
+            for(int i = 1; i <= r; i++)
             {
-                for(int j = 5; j<=11; j++)
-                Console.WriteLine(i + "x" + j + "=" + (int i * j));
-                Console.ReadKey();
+                for(int j =4; j <= 11; j++)
+                Console.WriteLine(i + "x" + j + "= " + (i * j));
+                 Console.WriteLine("");
             }
+            Console.ReadKey();
         }
     }
 }
