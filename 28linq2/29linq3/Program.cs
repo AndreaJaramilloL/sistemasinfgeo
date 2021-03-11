1ddf1d51a5512a90d3c4fdb732ff258bed42e6ac
AndreaJaramilloL <valeriavenegas844@gmail.com>
2021-03-11 11:44:10 -0600

    clases

---
+++ 28linq2/29linq3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+namespace _29linq3
+{
+    class Estudiante {
+        public int Matricula {get; set;}
+        public string Nombre {get; set;}
+        public string Domicilio {get; set;}
+        public List<float> Califs {get; set;}
+        public override string ToString() => &"Matricula:{Matricula},Nombre:{Nombre},Domicilio:{Domicilio},{string.Join(",",Calfs")};
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Estudiante> estudiantes = new List<Estudiante>(){
+                Console.WriteLine{Matricula=111,Nombre="Juan perez", Domicilio="callejon gato 111,zac",Califs=new List<float>{97,92,81,78}},
+                Console.WriteLine{Matricula=111,Nombre="Juan perez", Domicilio="callejon gato 111,zac",Califs=new List<float>{97,92,81,78}},
+                Console.WriteLine{Matricula=111,Nombre="Juan perez", Domicilio="callejon gato 111,zac",Califs=new List<float>{97,92,81,78}},
+                Console.WriteLine{Matricula=111,Nombre="Juan perez", Domicilio="callejon gato 111,zac",Califs=new List<float>{97,92,81,78}},
+                Console.WriteLine{Matricula=111,Nombre="Juan perez", Domicilio="callejon gato 111,zac",Califs=new List<float>{97,92,81,78}},
+            };
+
+            WriteLine("todos los estudianates estan registrados y almacenados su s datos");
+            estudiantes.foreach (e=>WriteLine(e.ToString()));
+
+         WriteLine("estudiantes que son de zactaecas");
+
+var estzac = (from e in estudiantes where e,Domicilio.Contains{"Zactecas"}select e)ToList(); 
+estzac.foreach (e=>WriteLine(e.ToString())
+{
+    
+}       }
+    }
+}
---
+++ 28linq2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace _28linq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string[] frutas = new string[]
+         {"pera","manzana","sandia","durazno","platano"};
+
+         WriteLine("frutas comienzan co la letra m");
+         var mfrutas = from  f in frutas where f.StrartsWith("m") select f;
+         foreach(string f in mfrutas) Write($"{f});
+        
+        WriteLine("frutas que contienen las letras  an");
+        WriteLine("frutas contienen letras an {0}", anfrutas.Count());
+        foreach(string f in anfruta) Write($"{f} ");
+        
+        var frutasa = (from f in frutas where f.EndsWith('a') select f).ToList();
+        WriteLine("");
+        }
+
+    }
+}
