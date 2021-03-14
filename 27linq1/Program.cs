1a4af9bbb5018eda275ff2a7e48efb6d85387b35
AndreaJaramilloL <valeriavenegas844@gmail.com>
2021-03-13 21:36:43 -0600

    linq

--- 27linq1/Program.cs
+++ 27linq1/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.linq;
 using static System.Console;
+using System.Linq;
 using System.Collections.Generic;
+
 namespace _27linq1
 {
     class Program
@@ -9,29 +10,32 @@ namespace _27linq1
         static void Main(string[] args)
         {   
             //1. fuente datos
-            int[] numeros = new int[] {10,25,-1,10,0,20,22,65,000,-4,28,1000,2000,-233}; 
+            int[] numeros = new int[] {10,25,-1,10,0,20,22,65,800,-4,28,1000,2000,-233}; 
 
             //imprimir numeros pares
             WriteLine("listado de numeros pares de manera convencional");
             for(int i=0; i<numeros.Length; i++){
-                inf(numeros[i]%2==0)
+                if((numeros[i]%2)==0)
                 Write($"{numeros[i]}");
             }
             //2. crear consulta para obtener numeros pares
-            var qryppares = from num in numeros where (num%2)==0 select num;
+            IEnumerable<int> qrypares = from num in numeros where (num%2)==0 select num;
+
             //3. ejecutar consulta
             WriteLine("estado de nueros pares usando linq");
-            foreach (it n in qrypares)
+            foreach(int n in qrypares)
+            Write($"{n}");
+          
+            
+            WriteLine("listado de numeros impares ");
+              var qryimpares = (from num in numeros where (num%2)!=0 select num).ToArray();
+              foreach(int n in qryimpares)
+             Write($"{n}"); 
 
-              wrileLine(n);
-
-              var qrypares = (from num in where (num%2)!=0 select num).ToArray();
-              foreach(int n in qryinpares)
-              Write($"{n}" );             
-
-              WriteLine("listado de numeros mayores a 100 uasando linq y listado en formato lista");
-              var mayores = (from num in numeros where (num>=20 && num<=1000) && num%2==0 select num).ToList();
-              mayores.foreach(n=>Write($"{n}"));
+                WriteLine("listadio de numeros mayores a 100 usando linq y usando formato de lista");           
+                var mayores = (from num in numeros where (num>=20 && num<=1000) && num%2==0 select num).ToList();
+                mayores.ForEach(n=>Write($"{n}"));
+                
         }
     }
 }
--- 28linq2/Program.cs
+++ 28linq2/Program.cs
@@ -13,15 +13,17 @@ namespace _28linq2
          {"pera","manzana","sandia","durazno","platano"};
 
          WriteLine("frutas comienzan co la letra m");
-         var mfrutas = from  f in frutas where f.StrartsWith("m") select f;
-         foreach(string f in mfrutas) Write($"{f});
-        
-        WriteLine("frutas que contienen las letras  an");
-        WriteLine("frutas contienen letras an {0}", anfrutas.Count());
-        foreach(string f in anfruta) Write($"{f} ");
-        
-        var frutasa = (from f in frutas where f.EndsWith('a') select f).ToList();
-        WriteLine("");
+         var mfrutas = from  f in frutas where f.StartsWith('m') select f;
+         foreach(string f in mfrutas) Write($"{f} ");
+
+         var anfrutas = from f in frutas where f.Contains("an") select f;
+         WriteLine("frutas que contiene letras an: {0}", anfrutas.Count());
+         foreach(string f in anfrutas) Write($"{f} ");
+
+         var frutasa = (from f in frutas where f.EndsWith('a')select f).ToList();
+         WriteLine("frutas que terminan con la letra a: {0}", frutasa.Count());
+         frutasa.ForEach(f=>Write($"{f} "));
+
         }
 
     }
